a21615bc45bce2face510eb5e9aba2a77f61e17c
Sina Sohangir <sina@nextfit.com>
2020-04-25 13:02:25 -0700

    Output logs when error in testing

--- conftest.py
+++ conftest.py
@@ -1,12 +1,59 @@
-import os
+
+import logging.config
 
 import pytest
+from tortoise.contrib.test import finalizer, env_initializer
 
-from tortoise.contrib.test import finalizer, initializer
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{asctime} {levelname} [{name}:{lineno}] {message}',
+            # 'format': '{asctime} {levelname} [{name}.{module}.{funcName}:{lineno}] {message}',
+            'style': '{',
+        },
+        'simple': {
+            'format': '{asctime} {levelname} {message}',
+            'style': '{',
+        },
+    },
+    'filters': {
+    },
+    'handlers': {
+        'null': {
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'tortoise': {
+            'level': 'DEBUG',
+        },
+        'aiosqlite': {
+            'level': 'INFO',
+        },
+        'asyncio': {
+            'level': 'INFO',
+        }
+    },
+    'root': {
+        'handlers': ['console', ],
+        'level': 'DEBUG',
+    },
+}
 
 
 @pytest.fixture(scope="session", autouse=True)
 def initialize_tests(request):
-    db_url = os.environ.get("TORTOISE_TEST_DB", "sqlite://:memory:")
-    initializer(["tests.testmodels"], db_url=db_url)
+    try:
+        logging.config.dictConfig(LOGGING)
+    except AttributeError as e:
+        print(e)
+
+    env_initializer()
     request.addfinalizer(finalizer)
